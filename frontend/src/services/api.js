ffebcb5ff12f761b9eb40c161f5de35780947dad
saints-gabriel <gabrieldossantosmartins05@gmail.com>
2026-09-15 14:49:53 -0300

    fix: endpoints

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000"
+    baseURL: "https://backend-app-corrida-p6qa.onrender.com/"
 });
 
 export async function criarAtividade(atividade) {
